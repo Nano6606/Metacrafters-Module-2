872eeb7fcbbc9c3844c97c977182928aea60a25b
Nano6606 <129884507+Nano6606@users.noreply.github.com>
2023-09-18 21:24:34 +0530

    Update mytoken.sol

--- mytoken.sol
+++ mytoken.sol
@@ -1,37 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract EnhancedToken {
-   
-    // Public variables to store token details
-    string public name = "Enhanced Meta Asset";
-    string public symbol = "EMA";
-    uint256 public totalSupply = 0;
-   
-    // Event declarations
-    event Minted(address indexed to, uint256 value);
-    event Burned(address indexed from, uint256 value);
-   
-    // Mapping of address to balance
-    mapping(address => uint256) public balanceOf;
-   
-    // Modifier to check sufficient balance
-    modifier hasSufficientBalance(address _address, uint256 _value) {
-        require(balanceOf[_address] >= _value, "Insufficient balance");
+contract MyToken {
+
+    // Events
+    event Minted(address indexed to, uint value);
+    event Burned(address indexed from, uint value);
+
+    // Public variables
+    string public tokenName = "META";
+    string public tokenAbbrv = "MTA";
+    uint public totalSupply;
+    address public owner;
+
+    // Mapping from address to balance
+    mapping(address => uint) public balances;
+
+    // Constructor
+    constructor(uint _initialSupply) {
+        owner = msg.sender;
+        totalSupply = _initialSupply;
+        balances[owner] = _initialSupply;
+    }
+
+    // Modifiers
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
         _;
     }
 
-    // Function to mint new tokens
-    function mintToken(address _recipient, uint256 _amount) public {
-        totalSupply += _amount;
-        balanceOf[_recipient] += _amount;
-        emit Minted(_recipient, _amount);
+    // Mint function
+    function mint(address _to, uint _value) public onlyOwner {
+        totalSupply += _value;
+        balances[_to] += _value;
+        emit Minted(_to, _value);
     }
-   
-    // Function to burn tokens
-    function burnToken(address _holder, uint256 _amount) public hasSufficientBalance(_holder, _amount) {
-        totalSupply -= _amount;
-        balanceOf[_holder] -= _amount;
-        emit Burned(_holder, _amount);
+
+    // Burn function
+    function burn(address _from, uint _value) public onlyOwner {
+        require(balances[_from] >= _value, "Insufficient balance");
+        totalSupply -= _value;
+        balances[_from] -= _value;
+        emit Burned(_from, _value);
     }
 }
